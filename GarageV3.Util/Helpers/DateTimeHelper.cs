5ae4c0543824cf59c2479cd211d7f8b72729f8c0
Mikael Klevengård <mickekleven@outlook.com>
2022-08-14 12:06:52 +0200

    add beautifyDate in DateTimeHelper

--- GarageV3.Util/Helpers/DateTimeHelper.cs
+++ GarageV3.Util/Helpers/DateTimeHelper.cs
@@ -1,7 +1,30 @@
-﻿namespace GarageV3.Util.Helpers
+﻿using System.Text;
+
+namespace GarageV3.Util.Helpers
 {
     public static class DateTimeHelper
     {
+        private static StringBuilder sb = new();
+
         public static DateTime GetCurrentDate(bool isUtc = false) => isUtc ? DateTime.UtcNow : DateTime.Now;
+
+
+        public static string beautifyDate(this DateTime value)
+        {
+            sb.Clear();
+
+            var diff = DateTime.Now - value;
+
+            var days = diff.Days;
+            var hours = diff.Hours;
+            var minutes = diff.Minutes;
+
+            if (diff.Days > 0) { sb.Append($"Dagar: {diff.Days} "); }
+            if (diff.Hours > 0) { sb.Append($"Timmar: {diff.Hours} "); }
+            if (diff.Minutes > 0) { sb.Append($"Minuter: {diff.Minutes} "); }
+
+            return sb.ToString();
+        }
+
     }
 }
