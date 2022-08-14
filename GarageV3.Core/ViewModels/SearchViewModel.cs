4cdf0953e16ae1e6aaa521ee70a8efc4360155d5
Mikael Klevengård <mickekleven@outlook.com>
2022-08-14 22:22:35 +0200

    Inprogress Search option

--- GarageV3.Core/ViewModels/SearchViewModel.cs
+++ GarageV3.Core/ViewModels/SearchViewModel.cs
@@ -8,10 +8,18 @@
     {
         public IEnumerable<VehicleViewModel> Vehicles { get; set; }
 
+        public VehicleViewModel Vehicle { get; set; } = new();
+
         public IEnumerable<OwnerViewModel> Owners { get; set; }
+
+        public OwnerViewModel Owner { get; set; } = new();
+
 
         //ToDo: Add MemberShipViewModel
         //public IEnumerable<MemberShipViewModel> MemberShips { get; set; }
+
+
+
 
         public AltSearch AltSearch { get; set; }
 
