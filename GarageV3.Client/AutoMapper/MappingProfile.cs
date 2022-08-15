f7dfed4457966f1d6ce466f378b32b41c42619b7
Mikael Klevengård <mickekleven@outlook.com>
2022-08-15 16:57:24 +0200

    Fix Search with model and brand

--- GarageV3.Client/AutoMapper/MappingProfile.cs
+++ GarageV3.Client/AutoMapper/MappingProfile.cs
@@ -15,18 +15,13 @@ namespace GarageV3.AutoMapper
             CreateMap<Vehicle, VehicleViewModel>()
                 .ForMember(dest => dest.VType, from => from.MapFrom(s => s.VehicleType.VType))
                 .ForMember(dest => dest.CarModel, from => from.MapFrom(s => s.Model))
-                .ForMember(dest => dest.OwnerId, from => from.MapFrom(s => s.OwnerId))
-                .ForMember(dest => dest.CarModel, from => from.MapFrom(s => s.Model));
+                .ForMember(dest => dest.OwnerId, from => from.MapFrom(s => s.OwnerId)).ReverseMap();
 
 
-            CreateMap<Vehicle, VehicleViewModel>()
-                .ForMember(dest => dest.VType, from => from.MapFrom(s => s.VehicleType.VType))
-                .ForMember(dest => dest.CarModel, from => from.MapFrom(s => s.Model)).ReverseMap();
+            CreateMap<ParkCarViewModel, Vehicle>()
+                .ForMember(dest => dest.Model, from => from.MapFrom(m => m.CarModel)).ReverseMap();
 
-
-            CreateMap<Vehicle, ParkCarViewModel>()
-                .ForMember(dest => dest.CarModel, from => from.MapFrom(m => m.Model));
-            CreateMap<Vehicle, ParkCarViewModel>().ReverseMap();
+            //CreateMap<Vehicle, ParkCarViewModel>().ReverseMap();
 
 
             CreateMap<TicketViewModel, ReceitViewModel>();
